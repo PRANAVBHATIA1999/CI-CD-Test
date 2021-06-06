086b67a1cdf03be796af92dc704e2e28aaa0fe9c
Pranav Bhatia <pranavbhatia431999@gmail.com>
2021-06-06 14:07:13 +0530

    Adding Project to repo

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Hi All, I am implementing CI-CD Pipeline</h1>
+        <h3> - Pranav</h3>
       </header>
     </div>
   );
